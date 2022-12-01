65cf60809e21ee9ea68cb69950afb44f2ea48bfb
Camila Magaña <95370375+CamilaMagana@users.noreply.github.com>
2022-12-01 11:36:15 -0600

    Fix: hastables

--- TablaHash/src/tablahash/TablaHash.java
+++ TablaHash/src/tablahash/TablaHash.java
@@ -14,7 +14,17 @@ public class TablaHash {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TablaHashADT<String, String> plantaFavorita = new TablaHashADT<>();
+        
+        plantaFavorita.add("Camila", "Savila");
+        plantaFavorita.add("Jose Antonio", "La de petroleo");
+        plantaFavorita.add("Emilio", "Lirio");
+        plantaFavorita.add("Gabo", "Flores de loto");
+        
+        plantaFavorita.remove("Camila");
+        System.out.println(plantaFavorita.valueOf("Camila"));
+        
+        System.out.println(plantaFavorita.valueOf("Emilio"));
     }
     
 }
--- TablaHash/src/tablahash/TablaHashADT.java
+++ TablaHash/src/tablahash/TablaHashADT.java
@@ -4,10 +4,78 @@
  */
 package tablahash;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  *
  * @author camil
+ * @param <K>
+ * @param <V>
  */
-public class TablaHashADT {
-    
+public class TablaHashADT<K, V> {
+     private class Entrada {
+        public K llave;
+        public V valor;
+        public int hash;
+
+        public Entrada(K llave, V valor) {
+            this.llave = llave;
+            this.valor = valor;
+            this.hash = llave.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object otherEntry) {
+            Entrada other = (Entrada) otherEntry;
+            if(this.hash != other.hash) return false;
+            return this.llave.equals(other.llave);
+        }
+    }
+
+    private int tamanio;
+    private ArrayList<Entrada>[] entradas;
+
+    public TablaHashADT(int tamanio) {
+        this.tamanio = tamanio;
+        this.entradas = new ArrayList[tamanio];
+        for(int i = 0; i < tamanio; i++) {
+            entradas[i] = new ArrayList<>();
+        }
+    }
+
+    private int normalizeIndex(int llaveHasheada) {
+        return (llaveHasheada & 0x7FFFFFFF) % tamanio;
+    }
+
+    public void add(K llave, V valor) {
+        Entrada entrada = new Entrada(llave, valor);
+        int indice = normalizeIndex(entrada.hash);
+        
+        int entradaIndice = entradas[indice].indexOf(entrada);
+        if (entradaIndice >= 0) {
+            entradas[indice].add(entradaIndice, entrada);
+        } else {
+            entradas[indice].add(entrada);
+        }
+    }
+
+    public V valueOf(K keyToSearch) {
+        int bucketIndex = normalizeIndex(keyToSearch.hashCode());
+        Entrada entradaAux = new Entrada(keyToSearch, null);
+        int entradaIndex = entradas[bucketIndex].indexOf(entradaAux);
+        
+        if(entradaIndex < 0) {
+            return null;
+        } else {
+            return entradas[bucketIndex].get(entradaIndex).valor;
+        }
+    }
+
+    public void remove(K llaveAEliminar) {
+        Entrada entradaAux = new Entrada(llaveAEliminar, null);
+        int bucketIndex = normalizeIndex(entradaAux.hashCode());
+        entradas[bucketIndex].remove(entradaAux);
+    }
+
 }
